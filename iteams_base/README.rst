c67dbfb1ae6135a102ef8cd64353065728910110
Rojo Valisoa <v.andrianantenaina@etechconsulting-mg.com>
2023-05-20 10:12:25 +0300

    [ADD]: add base module

---
+++ iteams_base/README.rst
@@ -0,0 +1,23 @@
+===========
+Iteams Base
+===========
+
+Base Resource planning for Iteam-$ Community
+
+Configuration
+=============
+
+To configure this module, you need to:
+
+#. Go to ...
+
+Usage
+=====
+
+To use this module, you need to:
+
+#. Go to ...
+
+
+Changelog
+=========
---
+++ iteams_base/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2023 Iteam-$ CO
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Iteams Base',
+    'description': """
+        Base Resource planning for Iteam-$ Community""",
+    'version': '16.0.0.0',
+    'license': 'AGPL-3',
+    'author': 'Iteam-$ CO',
+    'website': 'https://iteam-s.mg/',
+    'depends': [
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+}
